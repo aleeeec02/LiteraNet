a8e3bffd4ef6a0052a574e2845ed55fabed49b25
samuelmolina <samuelmolina281816@gmail.com>
2023-09-16 14:50:30 -0500

    Actualizacion de Listas Simples

--- LiteraNet/LiteraNet.cpp
+++ LiteraNet/LiteraNet.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "Lista.h"
+
+Lista <int>* coleccion = new Lista<int>();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int count = 0;
+	int elem, elementos;
+	do {
+		cout << "Ingrese un elemento: "; cin >> elem;
+		coleccion->agregarFinal(elem);
+		count++;
+	} while (count < 5);
+	for (int i = 0; i < 5; i++) {
+		elementos = coleccion->obtenerPos(i);
+		cout << elementos << endl;
+	}
 }
