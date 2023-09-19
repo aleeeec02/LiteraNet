478f729bee619bd481ccbcb77ac007004de7b835
samuelmolina <samuelmolina281816@gmail.com>
2023-09-19 13:29:48 -0500

    Actualizacion de Lista y Nodo para trabajar con objetos dinamicos

--- LiteraNet/Lista.h
+++ LiteraNet/Lista.h
@@ -1,156 +1,175 @@
-#pragma once
+﻿#include "Nodo.h"
 #include <functional>
 using namespace std;
 typedef unsigned int uint;
-template <typename T, T NADA = 0>
+template <class T>
 class Lista {
-	struct Nodo;
-	typedef function<int(T, T)> Comp;
-	Nodo* ini;
-	uint lon;
-	Comp comparar;
+private:
+    Nodo<T>* nodo;
+    typedef function <int(T, T)> Comp;
+    Nodo<T>* ini;
+    uint lon; // número de elementos en la lista
+
 public:
-	Lista() : ini(nullptr), lon(0), comparar([](T a, T b) {return a - b; }) {}
-	Lista(Comp comparar) : ini(nullptr), lon(0), comparar(comparar) {}
-	~Lista();
-	uint longitud();
-	bool esVacia();
-	void agregaInicial(T elem);
-	void agregarPos(T elem, uint pos);
-	void agregarFinal(T elem);
-	void modificarInicial(T elem);
-	void modificarPos(T elem, uint pos);
-	void modificarFinal(T elem);
-	void eliminaInicial();
-	void eliminaPos(uint pos);
-	void eliminaFinal();
-	T obtenerInicial();
-	T obtenerPos(uint pos);
-	T obtenerFinal();
-	T buscar(T elem);
+    Lista() : ini(nullptr), lon(0) { nodo = new Nodo<T>(); }
+    ~Lista();
+    Lista(Nodo<T>* pNodo) {
+        nodo = pNodo;
+    };
+    uint longitud();
+    bool esVacia();
+    void agregaInicial(T elem);
+    void agregarPos(T elem, uint pos);
+    void agregaFinal(T elem);
+    void modificarInicial(T elem);
+    void modificarPos(T elem, uint pos);
+    void modificarFinal(T elem);
+    void eliminaInicial();
+    void eliminaPos(uint pos);
+    void eliminaFinal();
+    T obtenerInicial();
+    T obtenerPos(uint pos);
+    T obtenerFinal();
+    T buscar(T elem);
+
+    void agregar(T pNodo); //sumar nodos a la lista
+    bool esvacia(void);
+    T cabeza(void); //retorna el dato del primer nodo
+    Lista* resto(void); //retorna el puntero al "resto" de la lista
+    //resto= lo que queda de la lista sin la cabeza
+    string toPrint(string p);
+    T suma(T i);
+    int size();
+    void borrar(void); //borra la cabeza
+    void borrar_last();//borra el ultimo
+    void concat(Lista<T>* l1);// le transfiere los datos de l1 a this
+    Lista<T>* copy(void);// hace una copia de la lista
+    void tomar(int n);//deja "vivos" los n primeros nodos y borra el resto
+    void addOrdenado(T d);// suma nodos ordenados de menor a mayor
+    bool esta(T d); //retorna true cuando d est� en la lista
+    void borrarDato(T d);// borra el nodo que contiene a d
+
 };
-
-template <typename T, T NADA>
-struct Lista<T, NADA>::Nodo {
-	T elem;
-	Nodo* sig;
-	Nodo(T elem = NADA, Nodo* sig = nullptr) : elem(elem), sig(sig) {}
-};
-template <typename T, T NADA>
-Lista<T, NADA>::~Lista() {
-	Nodo* aux = ini;
-	while (aux != nullptr) {
-		aux = ini;
-		ini = ini->sig;
-		delete aux;
-	}
+template <class T>
+uint Lista<T>::longitud() {
+    return lon;
 }
-template <typename T, T NADA>
-uint Lista<T, NADA>::longitud() {
-	return lon;
+template <class T>
+bool Lista<T>::esVacia() {
+    return lon == 0;
 }
-template <typename T, T NADA>
-bool Lista<T, NADA>::esVacia() {
-	return lon == 0;
+template <class T>
+void Lista<T>::agregaInicial(T elem) {
+    Nodo<T>* nuevo = new Nodo<T>(elem);
+    if (nuevo != nullptr) {
+        ini = nuevo;
+        lon++;
+    }
 }
-
-template <typename T, T NADA>
-void Lista<T, NADA>::agregaInicial(T elem) {
-	Nodo* nuevo = new Nodo(elem, ini);
-	if (nuevo != nullptr) {
-		ini = nuevo;
-		lon++;
-	}
+template <class T>
+T Lista<T>::buscar(T elem) {
+    Nodo<T>* aux = ini;
+    while (aux != nullptr) {
+        if (comparar(aux->elem, elem) == 0) {
+            return aux->elem;
+        }
+        aux = aux->sgte;
+    }
+    return 0;
 }
 
-template <typename T, T NADA>
-T Lista<T, NADA>::buscar(T elem) {
-	Nodo* aux = ini;
-	while (aux != nullptr) {
-		if (comparar(aux->elem, elem) == 0) {
-			return aux->elem;
-		}
-		aux = aux->sig;
-	}
-	return NADA;
+template <class T>
+void Lista<T>::agregarPos(T elem, uint pos) {
+    if (pos > lon) return;
+    if (pos == 0) {
+        agregaInicial(elem);
+    }
+    else {
+        Nodo<T>* aux = ini;
+        for (int i = 1; i < pos; i++) {
+            aux = aux->get_Sgte();
+        }
+        Nodo<T>* nuevo = new Nodo<T>(elem);
+        nuevo->set_Sgte(aux->get_Sgte());
+        if (nuevo != nullptr) {
+
+            aux->set_Sgte(nuevo);
+            lon++;
+        }
+    }
 }
-template <typename T, T NADA>
-void Lista<T, NADA>::eliminaInicial() {
-	if (lon > 0) {
-		Nodo* aux = ini;
-		ini = ini->sig;
-		delete aux;
-		lon--;
-	}
+template <class T>
+void Lista<T>::agregaFinal(T elem) {
+    agregarPos(elem, lon);
 }
 
-template <typename T, T NADA>
-void Lista<T, NADA>::eliminaPos(uint pos) {
-}
-template <typename T, T NADA>
-void Lista<T, NADA>::eliminaFinal() {
+template <class T>
+void Lista<T>::eliminaInicial() {
+    if (lon > 0) {
+        Nodo<T>* aux = ini;
+        ini = ini->sgte;
+        delete aux;
+        lon--;
+    }
 }
 
-template <typename T, T NADA>
-void Lista<T, NADA>::agregarPos(T elem, uint pos) {
-	if (pos > lon) return;
-	if (pos == 0) {
-		agregaInicial(elem);
-	}
-	else {
-		Nodo* aux = ini;
-		for (int i = 1; i < pos; i++) {
-			aux = aux->sig;
-		}
-		Nodo* nuevo = new Nodo(elem, aux->sig);
-		if (nuevo != nullptr) {
-			aux->sig = nuevo;
-			lon++;
-		}
-	}
+template <class T>
+void Lista<T>::eliminaPos(uint pos) {
 }
-template <typename T, T NADA>
-void Lista<T, NADA>::agregarFinal(T elem) {
-	agregarPos(elem, lon); 
+template <class T>
+void Lista<T>::eliminaFinal() {
 }
-template <typename T, T NADA>
-void Lista<T, NADA>::modificarInicial(T elem) {
-	if (lon > 0) {
-		ini->elem = elem;
-	}
+
+template <class T>
+void Lista<T>::modificarInicial(T elem) {
+    if (lon > 0) {
+        ini->elem = elem;
+    }
 }
-template <typename T, T NADA>
-void Lista<T, NADA>::modificarPos(T elem, uint pos) {
-	if (pos >= 0 && pos < lon) {
-		Nodo* aux = ini;
-		for (int i = 0; i < pos; i++) {
-			aux = aux->sig;
-		}
-		aux->elem = elem;
-	}
+
+template <class T>
+void Lista<T>::modificarPos(T elem, uint pos) {
+    if (pos >= 0 && pos < lon) {
+        Nodo<T>* aux = ini;
+        for (int i = 0; i < pos; i++) {
+            aux = aux->sgte;
+        }
+        aux->elem = elem;
+    }
 }
-template <typename T, T NADA>
-void Lista<T, NADA>::modificarFinal(T elem) {
-	modificar(elem, lon - 1);
+template <class T>
+void Lista<T>::modificarFinal(T elem) {
+    modificar(elem, lon - 1);
 }
-template <typename T, T NADA>
-T Lista<T, NADA>::obtenerInicial() {
-	return obtenerPos(0);
+
+template <class T>
+T Lista<T>::obtenerInicial() {
+    return obtenerPos(0);
 }
-template <typename T, T NADA>
-T Lista<T, NADA>::obtenerPos(uint pos) {
-	if (pos >= 0 && pos < lon) {
-		Nodo* aux = ini;
-		for (int i = 0; i < pos; i++) {
-			aux = aux->sig;
-		}
-		return aux->elem;
-	}
-	else {
-		return NADA;
-	}
+
+
+template <class T>
+T Lista<T>::obtenerPos(uint pos) {
+    if (pos >= 0 && pos < lon) {
+        Nodo<T>* aux = ini;
+        for (int i = 0; i < pos; i++) {
+            aux = aux->get_Sgte();
+        }
+        return aux->get_Elem();
+    }
+    else {
+        return NULL;
+    }
 }
-template <typename T, T NADA>
-T Lista<T, NADA>::obtenerFinal() {
-	return obtenerPos(lon - 1);
+template <class T>
+T Lista<T>::obtenerFinal() {
+    return obtenerPos(lon - 1);
+}
+
+template <class T>
+void Lista<T>::agregar(T d) //100
+{
+    Nodo<T>* nuevo = new Nodo<T>(d);
+    nuevo->set_Sgte(nodo);
+    nodo = nuevo;
 }
