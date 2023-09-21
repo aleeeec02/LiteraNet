14154d9c26c6d59329e92bad8e50b9bdea25abf2
aleeeec02 <conzahualpa@gmail.com>
2023-09-21 04:55:12 -0500

    Modificar Usuario.h y Persona.h para que sea compatible con libro, agregar cambios(minimos) y validaciones de entrada de datos a LiteraNet.cpp + otros cambios en general

--- LiteraNet/Usuario.h
+++ LiteraNet/Usuario.h
@@ -1,16 +1,66 @@
 #pragma once
+
+#include "Persona.h"
+#include "Libro.h"
+#include <fstream>
+#include <sstream> // permite identificar la separaciones de strings (en este caso comas para identificar usernames; manipular y dividir strings)
+#include <functional> //lambda
+
+#include <random> // para poder implementar mt19937(generador de números aleatorios y útil para algoritmo de Twister de Mersenne)
+#include <ctime> // para poder utilizar `time()`
+
+using namespace std;
+
+
+
 //Esta clase se creara de manera manual
 
 
 class Usuario : public Persona {
+private:
+	string direccion;
+	string userID;
+
+	// Generación de un ID de usuario único
+	void generarCodigoDeUsuario() {
+		mt19937 motor(static_cast<unsigned int>(time(nullptr)));
+		uniform_int_distribution<int> distribucion(0, 35);
+		stringstream ss;
+		const char alfanum[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+		for (int i = 0; i < 8; ++i) {
+			int indiceAleatorio = distribucion(motor);
+			ss << alfanum[indiceAleatorio];
+		}
+
+		userID = ss.str();
+	}
+
 public:
-	Usuario() :Persona() { this->direccion = ""; }
-	Usuario(string nom, string ape, int ed, int d, string cor, string direc):Persona(nom, ape, ed, d, cor), direccion(direc){}
+	Usuario() :Persona() {
+		this->direccion = " "; 
+		generarCodigoDeUsuario(); // Automáticamente crea un userID cuando un nuevo usuario es creado
+	}
+	Usuario(std::string nom, std::string ape, int ed, int d, std::string cor, std::string direc)
+		: Persona(nom, ape, ed, d, cor), direccion(direc) {
+	}
+
 	~Usuario(){}
 
 	void setDireccion(string direc) { this->direccion = direc; }
 	string getDireccion() { return this->direccion; }
 
-private:
-	string direccion;
+	string getDireccion() {
+		return this->direccion;
+	}
+
+	void setUserID(string ID) {
+		userID = ID;
+	}
+	
+	string getUserID() {
+		return userID;
+	}
+
+
 };
